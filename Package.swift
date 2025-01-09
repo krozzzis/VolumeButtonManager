0b82c3cc531e15ee9197b47732cc0a6e46873677
Nikita Shumov <schumov.nn@gmail.com>
2025-01-10 00:55:30 +0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
-// swift-tools-version: 5.1
+// swift-tools-version:5.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "VolumeButtonManager",
-	platforms: [
-		.iOS(.v13),
-	],
+	// platforms: [
+	// 	.iOS(.v13),
+	// ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -19,5 +19,6 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "VolumeButtonManager"),
-    ]
+    ],
+	swiftLanguageVersions: [.v5]
 )
